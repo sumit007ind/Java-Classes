3360242ac35b6314c2881f2fa5eaede3f7d66278
sumit007ind <sharmaraj81808@gmail.com>
2024-10-08 11:46:58 +0530

    Initial commit

---
+++ src/input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("please Enter some input");
+        System.out.println("You are going to see your result in the form of virtual code so, please enter your code ");
+        int rollno = input.nextInt();
+        System.out.println("Your roll no is " + rollno);
+
+    }
+}
---
+++ src/primitives.java
@@ -0,0 +1,12 @@
+public class primitives {
+    public static void main(String[] args) {
+   int rollno = 64;
+   char letter = 's';
+   float marks = 12.2f;
+   double largeDecimalNumber = 123.343;
+   long largeInteger = 123;
+   boolean check= false;
+
+
+    }
+}
