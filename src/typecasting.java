2977038a7aae898b1ab5c4c616185cc64e69cdb0
sumit007ind <sharmaraj81808@gmail.com>
2024-10-09 23:35:06 +0530

    learnt byte stream

--- src/typecasting.java
+++ src/typecasting.java
@@ -3,7 +3,20 @@ import java.util.Scanner;
 public class typecasting {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        float num = input.nextFloat();
-        System.out.println(num);
+//        float num = input.nextFloat();
+//        System.out.println(num);
+//          float num = (int)(50.1315f);
+//          System.out.println(num);
+//
+//          int a = 257;
+//          byte b = (byte)(a);
+//          System.out.println(a);
+        byte a= 50;
+        byte b= 40;
+        byte c= 30;
+        int d = (a+b+c)/2;
+        System.out.println(d);
+
+
     }
 }
