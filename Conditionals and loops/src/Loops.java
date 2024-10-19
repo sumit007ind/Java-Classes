9463b787f0b0091b49c3f1ec1fec6edb32f92907
sumit007ind <sharmaraj81808@gmail.com>
2024-10-19 23:18:43 +0530

    new for loop

--- Conditionals and loops/src/Loops.java
+++ Conditionals and loops/src/Loops.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Loops {
     public static void main(String[] args) {
-        
+//        for(int num = 1; num <= 15; num +=2){
+//            System.out.println(num);\\
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int num = 1; num <= n  ; num++) {
+            System.out.println(num + "");
+        }
+
+
+        }
+
     }
-}
+
