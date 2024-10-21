afaa7367a8e5029dfda72a43b62c32380a41ce34
sumit007ind <sharmaraj81808@gmail.com>
2024-10-22 01:04:05 +0530

    created CountNum.java

---
+++ src/CountNUM.java
@@ -0,0 +1,16 @@
+public class CountNUM {
+    public static void main(String[] args) {
+        int n= 11111111;
+
+        int count = 0;
+        while(n > 0){
+            int rem = n% 10;
+            if(rem == 1){
+                count++;
+
+            }
+            n = n/10;
+        }
+        System.out.println(count);
+    }
+}
