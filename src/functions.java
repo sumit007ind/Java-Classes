acd12316c6c6c11b4f026dd5ef374a7cda9bc774
sumit007ind <sharmaraj81808@gmail.com>
2024-12-12 20:34:01 +0530

    //

--- src/functions.java
+++ src/functions.java
@@ -4,7 +4,18 @@ import java.util.Scanner;
 public class functions {
 
     public static void main(String[] args) {
+      int ans =  sum2();
+        System.out.println(ans);
 
+    }
+    static int sum2(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the first number ");
+        int num1= in.nextInt();
+        System.out.println("Enter the second number ");
+        int num2 = in.nextInt();
+        int sum = num1 +num2;
+        return sum;
     }
     static void sum (){
         Scanner in = new Scanner(System.in);
@@ -12,7 +23,7 @@ public class functions {
         int num1= in.nextInt();
         System.out.println("Enter the second number ");
         int num2 = in.nextInt();
-        int sum = num1 - num2;
+        int sum = num1 +num2;
         System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
 
     }
