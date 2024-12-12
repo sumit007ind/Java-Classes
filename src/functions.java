dcd3296fc287c54e9a15ac9cf542d27d79bbafad
sumit007ind <sharmaraj81808@gmail.com>
2024-12-12 20:20:56 +0530

    new fuctions

---
+++ src/functions.java
@@ -0,0 +1,19 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class functions {
+
+    public static void main(String[] args) {
+
+    }
+    static void sum (){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the first number ");
+        int num1= in.nextInt();
+        System.out.println("Enter the second number ");
+        int num2 = in.nextInt();
+        int sum = num1 - num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+
+    }
+}
