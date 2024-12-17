924b57fea782054f9d40f417c8dd64e2a6650bbf
sumit007ind <sharmaraj81808@gmail.com>
2024-12-17 23:57:31 +0530

    ///

--- src/functions.java
+++ src/functions.java
@@ -22,7 +22,7 @@ public class functions {
         System.out.println("Enter the 1st number ");
         int num1= in.nextInt();
         System.out.println("Enter the 2nd number ");
-        int num2 = in.nextInt();:
+        int num2 = in.nextInt();
         int sum = num1 +num2;
         System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
 
