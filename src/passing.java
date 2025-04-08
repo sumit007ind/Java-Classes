f0ccd6dc75fa7c4f635adb3049309f885d0849b4
sumit007ind <sharmaraj81808@gmail.com>
2025-04-09 02:37:22 +0530

    new

---
+++ src/passing.java
@@ -0,0 +1,9 @@
+public class passing {
+    public static void main(String[] args) {
+        String name = "sumit sharma";
+        greet(name);
+    }
+     static void greet(String naam) {
+         System.out.println(naam);
+     }
+}
