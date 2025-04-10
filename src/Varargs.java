6244e98bc85e6b68e9437dc6790db5bc90c7dc71
sumit007ind <sharmaraj81808@gmail.com>
2025-04-11 01:52:19 +0530

    created VarArgs
    variable lenght arguments

---
+++ src/Varargs.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Varargs {
+    public static void main(String[] args) {
+         fun(2,34,5,566,55,6,6);
+    }
+    static void fun(int...v){
+        System.out.println(Arrays.toString(v));
+
+    }
+}
