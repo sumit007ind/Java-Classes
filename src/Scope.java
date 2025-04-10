bdc84e889e2ce17dfee69fbd3da2eb9d7f1506c1
sumit007ind <sharmaraj81808@gmail.com>
2025-04-11 01:33:48 +0530

    created scope of a function

---
+++ src/Scope.java
@@ -0,0 +1,11 @@
+public class Scope {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 30;
+        System.out.println(a+b);
+    }
+    static void random(int marks){
+        int a = 60;
+        System.out.println(marks);
+    }
+}
