35d2b5abd71aa76830f59da74b70005b7e411d9f
sumit007ind <sharmaraj81808@gmail.com>
2025-04-18 11:16:18 +0530

    SlidingWindowProtocol

---
+++ src/SlidingWindowProtocol.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class SlidingWindowProtocol {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        System.out.print("Enter the window size: ");
+        int windowSize = scanner.nextInt();
+
+
+        System.out.print("Enter total number of frames to transmit: ");
+        int totalFrames = scanner.nextInt();
+
+
+        int[] frames = new int[totalFrames];
+        for (int i = 0; i < totalFrames; i++) {
+            frames[i] = random.nextInt(100);
+        }
+
+        System.out.println("\nRandomly generated frames to send:");
+        System.out.println(Arrays.toString(frames));
+
+
+
+        int current = 0;
+        while (current < totalFrames) {
+            int end = Math.min(current + windowSize, totalFrames);
+            System.out.println("\nTransmitting window: Frames " + current + " to " + (end - 1));
+
+
+            for (int i = current; i < end; i++) {
+                System.out.println("Sent Frame " + i + ": " + frames[i]);
+            }
+
+
+            boolean ackLost = random.nextInt(10) < 2; // 20% chance of loss
+            if (ackLost) {
+                int lostFrame = random.nextInt(end - current) + current;
+                System.out.println("ACK lost for Frame " + lostFrame + ", retransmitting from Frame " + lostFrame);
+                current = lostFrame;
+            } else {
+                System.out.println("All frames acknowledged from " + current + " to " + (end - 1));
+                current = end;
+            }
+        }
+
+
+        System.out.println("\nAll frames transmitted and acknowledged. Program ends.");
+        scanner.close();
+    }
+}
---
+++ src/StopAndWaitProtocol.java
@@ -0,0 +1,47 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class StopAndWaitProtocol {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+
+        int totalFrames = random.nextInt(5) + 5;
+        System.out.println("Total Frames to Transmit: " + totalFrames);
+
+        int currentFrame = 1;
+
+        while (currentFrame <= totalFrames) {
+
+            System.out.println("Transmitting Frame " + currentFrame);
+
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+
+            boolean ackReceived = random.nextBoolean();
+
+            if (ackReceived) {
+                System.out.println("ACK received for Frame " + currentFrame);
+
+                currentFrame++;
+            } else {
+
+                System.out.println("ACK not received for Frame " + currentFrame + ". Retransmitting...");
+            }
+
+            System.out.println();
+        }
+
+
+        System.out.println("All frames transmitted successfully. Program ends.");
+        scanner.close();
+    }
+}
