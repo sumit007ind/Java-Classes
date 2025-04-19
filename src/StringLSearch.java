6b7051324fd3aef5beaf2bd8f9a048ef97d11c3b
sumit007ind <sharmaraj81808@gmail.com>
2025-04-19 21:43:39 +0530

    linear search with int and str

---
+++ src/StringLSearch.java
@@ -0,0 +1,23 @@
+public class StringLSearch {
+    public static void main(String[] args) {
+        String name="Sumit";
+        char target='h';
+
+        System.out.println(search(name,target));
+
+    }
+    static boolean search(String str ,char target){
+        if(str.length() ==0){
+            return false;
+
+        }
+        for (int i = 0; i < str.length(); i++) {
+            if(target == str.charAt(i)){
+                return true;
+            }
+
+
+        }
+        return false;
+    }
+}
