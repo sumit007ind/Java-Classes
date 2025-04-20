3d5c04daeb2b61abbb91517a94e85da9f0ac457d
sumit007ind <sharmaraj81808@gmail.com>
2025-04-21 01:05:29 +0530

    even tumber

---
+++ src/evendigits.java
@@ -0,0 +1,35 @@
+public class evendigits {
+    public static void main(String[] args) {
+        int[] nums ={12, 345, 2, 6 ,7896};
+        System.out.println( findNumber(nums));
+
+
+    }
+    static int findNumber(int[ ] nums){
+    int count = 0;
+    for (int num : nums){
+        if (even(num)){
+            count++;
+        }
+    }
+    return count;
+    }
+    static boolean even (int num ){
+        int numberofdigits =digits(num);
+        if (numberofdigits % 2 == 0){
+            return true;
+        }
+        return false;
+
+    }
+    static int digits(int num){
+        int count = 0;
+        while(num>0){
+            count++;
+            num=num/10;
+        }
+        return count;
+    }
+
+
+}
--- src/twoDARRAY.java
+++ src/twoDARRAY.java
@@ -1,25 +1,29 @@
+import java.util.Arrays;
+
 public class twoDARRAY {
     public static void main(String[] args) {
         int[][] arr = {
                 {1, 2, 3},
-                {4, 5, 6},
+                {4, 5, 9},
                 {7, 8, 9}
 
         };
-        int target=9;
-        System.out.println(search(arr , target));
+        int target=8;
+        int[] ans = search(arr,target);
+        System.out.println(Arrays.toString(ans));
 
     }
-    static int search(int[][] arr,int target){
+    static int[] search(int[][] arr,int target){
         for (int row = 0; row < arr.length ; row++) {
             for (int col = 0; col < arr[row].length; col++) {
-                if(arr[col][row]== target]){
-                    return
+                if(arr[col][row]== target){
+                    return new int[]  {row,col};
 
                 }
 
             }
 
         }
+        return null;
     }
 }
