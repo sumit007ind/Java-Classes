945e61001adfc7e6fff7c515375622ad6a240981
sumit007ind <sharmaraj81808@gmail.com>
2025-04-21 01:23:36 +0530

    max wealth number

---
+++ src/maxwealth.java
@@ -0,0 +1,21 @@
+public class maxwealth {
+    public static void main(String[] args) {
+
+    }
+    public int maximumWealth(int[][] accounts) {
+        int ans = Integer.MIN_VALUE;
+        for (int person = 0; person < accounts.length; person++) {
+            int sum = 0;
+            for (int account = 0; account <accounts[person].length; account++) {
+                sum+=accounts[person][account];
+            }
+            if(sum >ans){
+                ans=sum;
+            }
+
+
+        }
+        return ans;
+
+    }
+}
