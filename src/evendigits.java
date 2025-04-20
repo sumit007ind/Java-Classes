335bdcfed73bfdea2bb17d9ce33e06b694c2a08b
sumit007ind <sharmaraj81808@gmail.com>
2025-04-21 01:35:26 +0530

    ,,,

--- src/evendigits.java
+++ src/evendigits.java
@@ -1,6 +1,6 @@
 public class evendigits {
     public static void main(String[] args) {
-        int[] nums ={12, 345, 2, 6 ,7896};
+        int[] nums ={12, 345, 2, 6 ,7896 ,6666};
         System.out.println( findNumber(nums));
 
 
