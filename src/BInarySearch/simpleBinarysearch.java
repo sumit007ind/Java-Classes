ee6e0609ed28c4e8733fe73b465f5b25a3c28760
sumit007ind <sharmaraj81808@gmail.com>
2025-04-24 00:46:15 +0530

    binary search

--- src/BInarySearch/simpleBinarysearch.java
+++ src/BInarySearch/simpleBinarysearch.java
@@ -5,6 +5,11 @@ public class simpleBinarysearch {
 
     }
     static int binarySearch(int[] arr, int target){
-        
+        int start = 0;
+        int end = arr.length-1;
+        while(start <=end){
+            int mid = start +(end-start)/2;
+
+        }
     }
 }
