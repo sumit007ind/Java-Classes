43911e544509b957e118066df5d7632af7a3558c
sumit007ind <sharmaraj81808@gmail.com>
2025-07-17 02:18:07 +0530

    bgbc

--- src/PatternQuestion/Pattern3.java
+++ src/PatternQuestion/Pattern3.java
@@ -6,8 +6,8 @@ public class Pattern3 {
     }
     static void pattern4 ( int n){
         for (int row = 1; row <=n; row++) {
-            for (int col = 1; col <=n-row+1; col++) {
-                System.out.print( col );
+            for (int col = row; col <=n; col++) {
+                System.out.print( "* " );
 
             }
             System.out.println();
