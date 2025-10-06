fc9cba4a07c87e1adad5b0b4cd91898ee81ea72d
sumit007ind <sharmaraj81808@gmail.com>
2025-10-06 15:24:47 +0530

    First Stack example

---
+++ src/StackandQueues/FirstExample.java
@@ -0,0 +1,16 @@
+package StackandQueues;
+
+import java.util.Stack;
+
+public class FirstExample {
+    public static void main(String[] args) {
+        Stack<Integer> Stack  = new Stack<>();
+        Stack.push(12);
+        Stack.push(13);
+        Stack.push(14);
+        System.out.println(Stack.pop());
+        System.out.println(Stack.pop());
+        System.out.println(Stack.pop() );
+
+    }
+}
